03e34030825cf797b8ac8e27ccb1956a36f9c7b1
nitaku <matteo.abrate@gmail.com>
2024-03-02 22:44:24 +0100

    service worker: offline copy of pages

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,57 +1,18 @@
-'use strict';
+// This is the "Offline copy of pages" service worker
 
-// Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE = "pwabuilder-offline";
 
-// Add list of files to cache here.
-const FILES_TO_CACHE = [
-  '/offline.html',
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-
-  evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-      })
-  );
-
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // Remove previous cached data from disk.
-  evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // Add fetch event handler here.
-  if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
-    return;
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
   }
-  evt.respondWith(
-      fetch(evt.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                  return cache.match('offline.html');
-                });
-          })
-  );
 });
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
